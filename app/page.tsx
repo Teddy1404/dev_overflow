c0807e37b00929b299311b3661d99613a494440b
Teddy1404 <aryansinghchandel1404@gmail.com>
2024-01-09 14:14:36 +0530

    auth routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-   <h1>
-    hello
-   </h1>
+   <div>
+    <UserButton afterSignOutUrl="/">
+
+    </UserButton>
+   </div>
   )
 }
